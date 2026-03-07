d42e17af99858200522b65c32278674e79a83a1d
Bikal Gurung <gbikal@gmail.com>
2026-03-07 21:48:32 +0000

    Add 100KB, 1MB, 10MB, 100MB benchmarks for all codecs
    Extend decompress and compress benchmarks for flate, gzip, zlib, and
    LZW to cover 100KB through 100MB sizes, with matching Go benchmarks
    for comparison.

--- tools/bench_test.go
+++ tools/bench_test.go
@@ -269,6 +269,70 @@ func BenchmarkFlateDecompress_10mb(b *testing.B) {
 	}
 }
 
+func BenchmarkFlateDecompress_100mb(b *testing.B) {
+	data := genText(104857600)
+	var cbuf bytes.Buffer
+	w, _ := flate.NewWriter(&cbuf, flate.DefaultCompression)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := flate.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkFlateCompressDefault_100kb(b *testing.B) {
+	data := genText(102400)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w, _ := flate.NewWriter(&buf, flate.DefaultCompression)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkFlateCompressDefault_1mb(b *testing.B) {
+	data := genText(1048576)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w, _ := flate.NewWriter(&buf, flate.DefaultCompression)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkFlateCompressDefault_10mb(b *testing.B) {
+	data := genText(10485760)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w, _ := flate.NewWriter(&buf, flate.DefaultCompression)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkFlateCompressDefault_100mb(b *testing.B) {
+	data := genText(104857600)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w, _ := flate.NewWriter(&buf, flate.DefaultCompression)
+		w.Write(data)
+		w.Close()
+	}
+}
+
 // --- gzip ---
 
 func BenchmarkGzipCompressDefault_1kb(b *testing.B) {
@@ -327,6 +391,118 @@ func BenchmarkGzipDecompress_10kb(b *testing.B) {
 	}
 }
 
+func BenchmarkGzipDecompress_100kb(b *testing.B) {
+	data := genText(102400)
+	var cbuf bytes.Buffer
+	w := gzip.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := gzip.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkGzipDecompress_1mb(b *testing.B) {
+	data := genText(1048576)
+	var cbuf bytes.Buffer
+	w := gzip.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := gzip.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkGzipDecompress_10mb(b *testing.B) {
+	data := genText(10485760)
+	var cbuf bytes.Buffer
+	w := gzip.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := gzip.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkGzipDecompress_100mb(b *testing.B) {
+	data := genText(104857600)
+	var cbuf bytes.Buffer
+	w := gzip.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := gzip.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkGzipCompressDefault_100kb(b *testing.B) {
+	data := genText(102400)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkGzipCompressDefault_1mb(b *testing.B) {
+	data := genText(1048576)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkGzipCompressDefault_10mb(b *testing.B) {
+	data := genText(10485760)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkGzipCompressDefault_100mb(b *testing.B) {
+	data := genText(104857600)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
 // --- zlib ---
 
 func BenchmarkZlibCompressDefault_10kb(b *testing.B) {
@@ -354,6 +530,118 @@ func BenchmarkZlibDecompress_10kb(b *testing.B) {
 		r, _ := zlib.NewReader(bytes.NewReader(compressed))
 		io.ReadAll(r)
 		r.Close()
+	}
+}
+
+func BenchmarkZlibDecompress_100kb(b *testing.B) {
+	data := genText(102400)
+	var cbuf bytes.Buffer
+	w := zlib.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := zlib.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkZlibDecompress_1mb(b *testing.B) {
+	data := genText(1048576)
+	var cbuf bytes.Buffer
+	w := zlib.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := zlib.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkZlibDecompress_10mb(b *testing.B) {
+	data := genText(10485760)
+	var cbuf bytes.Buffer
+	w := zlib.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := zlib.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkZlibDecompress_100mb(b *testing.B) {
+	data := genText(104857600)
+	var cbuf bytes.Buffer
+	w := zlib.NewWriter(&cbuf)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r, _ := zlib.NewReader(bytes.NewReader(compressed))
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkZlibCompressDefault_100kb(b *testing.B) {
+	data := genText(102400)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkZlibCompressDefault_1mb(b *testing.B) {
+	data := genText(1048576)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkZlibCompressDefault_10mb(b *testing.B) {
+	data := genText(10485760)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkZlibCompressDefault_100mb(b *testing.B) {
+	data := genText(104857600)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write(data)
+		w.Close()
 	}
 }
 
@@ -412,6 +700,118 @@ func BenchmarkLzwDecompressLSB_10kb(b *testing.B) {
 		r := lzw.NewReader(bytes.NewReader(compressed), lzw.LSB, 8)
 		io.ReadAll(r)
 		r.Close()
+	}
+}
+
+func BenchmarkLzwDecompressLSB_100kb(b *testing.B) {
+	data := genText(102400)
+	var cbuf bytes.Buffer
+	w := lzw.NewWriter(&cbuf, lzw.LSB, 8)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := lzw.NewReader(bytes.NewReader(compressed), lzw.LSB, 8)
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkLzwDecompressLSB_1mb(b *testing.B) {
+	data := genText(1048576)
+	var cbuf bytes.Buffer
+	w := lzw.NewWriter(&cbuf, lzw.LSB, 8)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := lzw.NewReader(bytes.NewReader(compressed), lzw.LSB, 8)
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkLzwDecompressLSB_10mb(b *testing.B) {
+	data := genText(10485760)
+	var cbuf bytes.Buffer
+	w := lzw.NewWriter(&cbuf, lzw.LSB, 8)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := lzw.NewReader(bytes.NewReader(compressed), lzw.LSB, 8)
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkLzwDecompressLSB_100mb(b *testing.B) {
+	data := genText(104857600)
+	var cbuf bytes.Buffer
+	w := lzw.NewWriter(&cbuf, lzw.LSB, 8)
+	w.Write(data)
+	w.Close()
+	compressed := cbuf.Bytes()
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r := lzw.NewReader(bytes.NewReader(compressed), lzw.LSB, 8)
+		io.ReadAll(r)
+		r.Close()
+	}
+}
+
+func BenchmarkLzwCompressLSB_100kb(b *testing.B) {
+	data := genText(102400)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := lzw.NewWriter(&buf, lzw.LSB, 8)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkLzwCompressLSB_1mb(b *testing.B) {
+	data := genText(1048576)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := lzw.NewWriter(&buf, lzw.LSB, 8)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkLzwCompressLSB_10mb(b *testing.B) {
+	data := genText(10485760)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := lzw.NewWriter(&buf, lzw.LSB, 8)
+		w.Write(data)
+		w.Close()
+	}
+}
+
+func BenchmarkLzwCompressLSB_100mb(b *testing.B) {
+	data := genText(104857600)
+	b.SetBytes(int64(len(data)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := lzw.NewWriter(&buf, lzw.LSB, 8)
+		w.Write(data)
+		w.Close()
 	}
 }
 
